16938f1fdcd1db3f6e5bf91eb78877bf39c6b60d
Kumar, Bharat <bharat.e.kumar@accenture.com>
2026-02-15 11:56:30 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = {
+  output: {
+    uniqueName: 'lab',
+    publicPath: 'auto',
+    module: true, // ✅ required for ESM
+    scriptType: 'module',
+  },
+  experiments: {
+   outputModule: true, // ✅ required for ESM
+  },
+  ...withModuleFederationPlugin({
+    library: { type: 'module' }, // ✅ required for ESM
+
+  name: 'lab',
+    filename: 'ngxp-remoteEntry.js',
+
+    exposes: {
+      './AppModule': './src/app/app.module.ts',
+    },
+
+    shared: {
+      ...shareAll({
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: 'auto',
+      }),
+    },
+  }),
+};
