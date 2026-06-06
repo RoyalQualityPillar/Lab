52edb6382bd508bc7a61530b8c1f2b1de805b794
Royal <RQPAdmin3@Saanso-QMS-Prod.>
2026-06-06 18:11:42 +0530

    added

---
+++ src/app/rqp-lims-module/ism/ism-completed-save/ism-completed-save.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ism-completed-save',
+  standalone: false,
+  templateUrl: './ism-completed-save.component.html',
+  styleUrl: './ism-completed-save.component.scss'
+})
+export class IsmCompletedSaveComponent {
+
+}
