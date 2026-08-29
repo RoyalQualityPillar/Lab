3bac6801946c022e737d69fe00adb077f9955883
rqpadmin15 <Sales@Royalqualitypillars.com>
2026-08-29 17:31:54 +0530

    created download buttons

--- src/app/rqp-lims-cm-module/lims-cm-master/cci-master/cci-master.service.ts
+++ src/app/rqp-lims-cm-module/lims-cm-master/cci-master/cci-master.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -9,4 +10,17 @@ import { environment } from 'src/environments/environment';
 export class CciMasterService {
   private API_URL = environment.apiBaseURL;
   constructor(private http: HttpClient, private cookieService: CookieService) {}
+
+    generateReport(
+          uc0001: any,
+          templateName: string,
+          moduleCode: string
+        ): Observable<any> {
+          return this.http.post(
+            this.API_URL +
+              `limscm/cci-master-report?uc0001=${uc0001}&templateName=${templateName}&moduleCode=${moduleCode}`,
+            ''
+          );
+        
+          }
 }
