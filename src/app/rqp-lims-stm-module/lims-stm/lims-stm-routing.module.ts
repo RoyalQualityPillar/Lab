53717e4e5326237fc7a04d7c3a016f297a6af73c
rqpadmin15 <Sales@Royalqualitypillars.com>
2026-09-18 12:56:28 +0530

    commit

--- src/app/rqp-lims-stm-module/lims-stm/lims-stm-routing.module.ts
+++ src/app/rqp-lims-stm-module/lims-stm/lims-stm-routing.module.ts
@@ -3,37 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { StmModuleAdminComponent } from '../stm-module-admin/stm-module-admin.component';
 import { StorageCategoryMasterHomePageComponent } from '../stm-masters/storage-category-master/storage-category-master-home-page/storage-category-master-home-page.component';
 import { ChambersMasterHomePageComponent } from '../stm-masters/chambers-master/chambers-master-home-page/chambers-master-home-page.component';
-<<<<<<< HEAD
-import { StudyTypeMasterHomePageComponent } from '../stm-masters/study-type-master/study-type-master-home-page/study-type-master-home-page.component';
-import { StorageConditioMasterHomePageComponent } from '../stm-masters/storage-condition-master/storage-conditio-master-home-page/storage-conditio-master-home-page.component';
-import { ChambersTypeMasterHomePageComponent } from '../stm-masters/chambers-type-master/chambers-type-master-home-page/chambers-type-master-home-page.component';
-import { ChambersRacksMasterHomePageComponent } from '../stm-masters/chambers-racks-master/chambers-racks-master-home-page/chambers-racks-master-home-page.component';
-import { ChambersShellMasterHomePageComponent } from '../stm-masters/chambers-shell-master/chambers-shell-master-home-page/chambers-shell-master-home-page.component';
-import { StabilityProtRecHomePageComponent } from '../stm-masters/stability-protocol-record/stability-prot-rec-home-page/stability-prot-rec-home-page.component';
-import { StabilityProtScheHomePageComponent } from '../stm-masters/stability-protocol-schedule/stability-prot-sche-home-page/stability-prot-sche-home-page.component';
-import { StabilityProtScheRecHomePageComponent } from '../stm-masters/stability-protocol-schedule-record/stability-prot-sche-rec-home-page/stability-prot-sche-rec-home-page.component';
-
-
-const routes: Routes = [
-  { path: 'stm-module-admin', component: StmModuleAdminComponent },
-  { path: 'storage-category-master-home-page', component: StorageCategoryMasterHomePageComponent },
-  { path: 'chambers-master-home-page', component: ChambersMasterHomePageComponent },
-  { path: 'stydy-type-master-home-page', component: StudyTypeMasterHomePageComponent },
-  { path: 'storage-conditio-master-home-page', component: StorageConditioMasterHomePageComponent },
-  { path: 'chambers-type-master-home-page', component: ChambersTypeMasterHomePageComponent },
-  { path: 'chambers-racks-master-home-page', component: ChambersRacksMasterHomePageComponent },
-  { path: 'chambers-shell-master-home-page', component: ChambersShellMasterHomePageComponent },
-  { path: 'stability-prot-rec-home-page', component: StabilityProtRecHomePageComponent },
-  { path: 'stability-prot-sche-home-page', component: StabilityProtScheHomePageComponent },
-  { path: 'stability-prot-sche-rec-home-page', component: StabilityProtScheRecHomePageComponent },
-
-
-
-
-
-
-
-=======
 import { WsPeramentersHomePageComponent } from '../stm-masters/ws-peramenters-master/ws-peramenters-home-page/ws-peramenters-home-page.component';
 import { WsTemplateIndexHomePageComponent } from '../stm-masters/ws-template-index/ws-template-index-home-page/ws-template-index-home-page.component';
 import { WsPeraamentersRecordHomePageComponent } from '../stm-masters/ws-peraamenters-record/ws-peraamenters-record-home-page/ws-peraamenters-record-home-page.component';
@@ -45,7 +14,6 @@ const routes: Routes = [
 {path:'ws-peramenters-home-page', component: WsPeramentersHomePageComponent },
 {path:'ws-template-index-home-page', component: WsTemplateIndexHomePageComponent },
 {path:'ws-peraamenters-record-home-page', component: WsPeraamentersRecordHomePageComponent },
->>>>>>> ba4e149242966e05cb8433efed5511b99a615203
 
 
 
